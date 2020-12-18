76e7a4859f92f1a77d85771d05f882b203aaa7c8
KirtiGautam <gautamkirti8c@gmail.com>
2020-12-18 22:02:47 +0530

    static files

--- fyle/settings.py
+++ fyle/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend/build/static"),
     os.path.join(BASE_DIR, "frontend/build"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "frontend/build/static")
